aa07b4e1d5a00fa03d0443ebdb858711b4ac0fa9
derek yu <yuderekyu@gmail.com>
2022-02-14 11:34:38 -0600

    [3.4.5] complete longest_line using multiple functions

--- 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
+++ 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
@@ -16,9 +16,23 @@ defmodule LineHelper do
   def longest_line_length!(path) do
     # read file
     File.stream!(path)
+    # remove trailing newline character from each line
+    |> Stream.map(&String.replace(&1, "\n", ""))
     # map to a list of lengths
-    |>
+    |> Stream.map(&String.length/1)
     # store the maximum and iterate through list.
-    # if current is > maximum, update the maximum
+    # if current is > maximum, update the maximum to the current
+    |> Enum.reduce(0, &get_largest_number/2)
+  end
+
+  defp get_largest_number(current, result) when current > result do
+    current
+  end
+
+  defp get_largest_number(_, result) do
+    result
   end
 end
+
+# LineHelper.lines_lengths!("./line_helper.ex")
+# LineHelper.longest_line_length!("./line_helper.ex")
