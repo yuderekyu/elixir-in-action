493d1bc550d0564de7d9cedef0ca0e7fa10f791d
derek yu <yuderekyu@gmail.com>
2022-02-14 13:25:38 -0600

    3.4.5 - initialize longest_line!

--- 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
+++ 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
@@ -32,7 +32,32 @@ defmodule LineHelper do
   defp get_largest_number(_, result) do
     result
   end
+
+  # return the contents of the longest line in a file
+  def longest_line!(path) do
+	  # read in file
+    File.stream!(path)
+    # remove trailing newline character
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    # create a list of tuples that contain content and length?
+    |> Stream.map(&create_content_length_tuple/1)
+    # create an enumerable that contains the current element and index
+    |> Enum.reduce(%{content: "", length: 0}, &get_longest_line/2)
+  end
+
+  defp create_content_length_tuple(element) do
+    %{content: element, length: String.length(element)}
+  end
+
+  defp get_longest_line(current, result) when current.length > result.length do
+   %{content: current.content, length: current.length}
+  end
+
+  defp get_longest_line(_, result) do
+    result
+  end
 end
 
 # LineHelper.lines_lengths!("./line_helper.ex")
 # LineHelper.longest_line_length!("./line_helper.ex")
+# LineHelper.longest_line!("./line_helper.ex")
