16a99d67307a9f0fdcd8dd341941a131373bb0b2
derek yu <yuderekyu@gmail.com>
2022-02-14 22:27:20 -0600

    3.4.5 complete exercises

--- 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/enum_streams.ex
+++ 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/enum_streams.ex
@@ -1,3 +1,4 @@
+# official solutions
 defmodule EnumStreams do
 	def filtered_lines!(path) do
     path
@@ -10,6 +11,34 @@ defmodule EnumStreams do
   # |> &filtered_lines!/1 # this won't work, it creates an anon function, rather than calling it.
     |> filtered_lines!()
     |> Enum.map(&String.length/1)
+  end
+
+  # returns the length of the longest line in a file
+  def longest_line_length!(path) do
+    path
+    |> filtered_lines!()
+    |> Stream.map(&String.length/1)
+    |> Enum.max() # what!
+  end
+
+  # returns contents of the longest line in a file
+  def longest_line!(path) do
+    path
+    |> filtered_lines!()
+    |> Enum.max_by(&String.length/1) # woah
+  end
+
+  # returns a list of numbers, with each number represnting the word count in a file
+  def words_per_line!(path) do
+    path
+    |> filtered_lines!()
+    |> Enum.map(&word_count/1)
+  end
+
+  defp word_count(string) do
+    string
+    |> String.split()
+    |> length()
   end
 end
 
--- 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
+++ 3-control-flow/3.4-loops-and-iterations/3.4.5-streams/practice/line_helper.ex
@@ -1,28 +1,24 @@
-# my trash attempts lol
+# my attempts lol
 defmodule LineHelper do
-	# take a file path and return a list of numbers, with each number representing the length of the corresponding line from the file
-  def lines_lengths!(path) do
+  defp filtered_lines!(path) do
     # read the file
     File.stream!(path)
-
     # remove trailing newline character from each line
     |> Stream.map(&String.replace(&1, "\n", ""))
-    # get the length of each line
+  end
 
-    # |> Enum.map(&String.length(&1))
+	# take a file path and return a list of numbers, with each number representing the length of the corresponding line from the file
+  def lines_lengths!(path) do
+    path
+    |> filtered_lines!()
     |> Enum.map(&String.length/1) # directly reference the fn w/o specifying the nth argument
   end
 
   # takes a file path and returns the length of the longest line in a file
   def longest_line_length!(path) do
-    # read file
-    File.stream!(path)
-    # remove trailing newline character from each line
-    |> Stream.map(&String.replace(&1, "\n", ""))
-    # map to a list of lengths
+    path
+    |> filtered_lines!()
     |> Stream.map(&String.length/1)
-    # store the maximum and iterate through list.
-    # if current is > maximum, update the maximum to the current
     |> Enum.reduce(0, &get_largest_number/2)
   end
 
@@ -36,31 +32,41 @@ defmodule LineHelper do
 
   # return the contents of the longest line in a file
   def longest_line!(path) do
-    File.stream!(path)
-    |> Stream.map(&String.replace(&1, "\n", ""))
-    |> Stream.map(&create_content_length_tuple/1)
-    |> Enum.reduce(%{content: "", length: 0}, &get_longest_line/2)
+    path
+    |> filtered_lines!()
+    |> Stream.map(&create_content_length_map/1)
+    |> Enum.reduce(%{content: "", length: 0}, &get_longest_line_from_map/2)
+
     # remote function call (defined in another module). stylistically, avoid List and Map, b/c there are built in kernel fns that can do the same things
     # |> Map.get(:content)
-    # kernel equivalent
+    # use kernel equivalent
     |> get_in([:content])
   end
 
-  defp create_content_length_tuple(element) do
+  defp create_content_length_map(element) do
     %{content: element, length: String.length(element)}
   end
 
-  defp get_longest_line(current, result) when current.length > result.length do
+  # if's are nice when a guard doesn't handle the case well
+  # otherwise, use pattern matching with functions
+  defp get_longest_line_from_map(current, result) when current.length > result.length do
    %{content: current.content, length: current.length}
   end
 
-  defp get_longest_line(_, result) do
+  defp get_longest_line_from_map(_, result) do
     result
   end
 
-  # if's are nice when a guard doesn't handle the case well
+  # returns a list of numbers, with each number representing the word count in a file
+  def words_per_line!(path) do
+    path
+    |> filtered_lines!()
+    # map, split line by space?
+    |> Enum.map(&(length(String.split(&1)))) # extract this into a separate function
+  end
 end
 
 # LineHelper.lines_lengths!("./line_helper.ex")
 # LineHelper.longest_line_length!("./line_helper.ex")
 # LineHelper.longest_line!("./line_helper.ex")
+# LineHelper.words_per_line!("./line_helper.ex")
