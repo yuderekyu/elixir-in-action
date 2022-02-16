b5843c2fa1b7f9cf8debbd3a8941db84f8094c61
derek yu <yuderekyu@gmail.com>
2022-02-16 15:26:33 -0600

    [4] structs vs maps

--- 4-data-abstractions/4.1.4-abstracting-with-structs/fraction.ex
+++ 4-data-abstractions/4.1.4-abstracting-with-structs/fraction.ex
@@ -23,3 +23,8 @@ one_half = Fraction.new(1,2)
 
 # pattern match
 %Fraction{a: a1, b: b2} = one_half
+
+# a struct is a special kind of map.
+# a map can pattern match to a struct, but not the other way around.
+%Fraction{} = %{a: 1, b: 2} # error
+%{a: a, b: b} = Fraction{a: 1, b: 2} # matches
